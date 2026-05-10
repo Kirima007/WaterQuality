9993b9bba3ea2e863680fa2a537cde3d4e1e8153
Kirima <marid0620915353@gmail.com>
2026-05-11 01:16:37 +0700

    FIX SOMETHING

--- src/Config.h
+++ src/Config.h
@@ -4,12 +4,12 @@
 #define ENC_CLK 32
 #define ENC_DT 33
 #define ENC_SW 14
-// #define RGB_R 9
-// #define RGB_G 13
-// #define RGB_B 12
-#define RGB_R 15
+#define RGB_R 9
 #define RGB_G 13
 #define RGB_B 12
+// #define RGB_R 15
+// #define RGB_G 13
+// #define RGB_B 12
 #define BUZZER 2
 #define ONE_WIRE 18
 #define GPS_RX 19
